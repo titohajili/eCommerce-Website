ecff8c88cb19469620613b529bd52a23eea60276
titohajili <maqatoto963@gmail.com>
2024-03-19 16:14:40 +0400

    Notification Toast

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,13 @@ const modalCloseFunc = function () {modal.classList.add('closed')}
 //modal eventListener
 modalCloseOverlay.addEventListener('click', modalCloseFunc);
 moadalCloseBtn.addEventListener('click', modalCloseFunc);
+
+
+//Notification Toast Variables
+const notificationToast = document.querySelector('[data-toast]');
+const toastCloseBtn = document.querySelector('[data-toast-close]');
+
+//Notification Toast EcentListener
+toastCloseBtn.addEventListener('click', function () {
+    notificationToast.classList.add('closed');
+})
